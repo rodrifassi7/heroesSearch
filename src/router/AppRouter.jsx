59b8d5bcb9b27010b2f4221955bcaea2b4225260
Rodrigo Fassi <rodrifassi7@gmail.com>
2024-04-19 15:20:01 -0300

    second

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import { LoginPages } from "../auth";
+import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
+
+export const AppRouter = () => {
+  return (
+    <>
+
+      <Routes>
+
+        <Route path="login" element={<LoginPages />} />
+
+        <Route path="/*" element={<HeroesRoutes />} />
+
+      </Routes>
+    </>
+  );
+};
