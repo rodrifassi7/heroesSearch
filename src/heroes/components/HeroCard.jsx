62c5e91285b1b7f9c7b4f7acd7725514eb146740
Rodrigo Fassi <rodrifassi7@gmail.com>
2024-04-22 14:38:42 -0300

    todo: fix responsive

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -18,28 +18,27 @@ export const HeroCard = ({
 
   return (
     <div className="col animate__animated animate__fadeIn">
-      <div className="card">
-        <div className="row no-gutters">
-          <div className="col-4">
-
-          
-            <img src={heroImgUrl} alt={superhero} className="card-img" />
-          </div>
-
-          <div className="col-8">
-            <div className="card-body">
-              <h5 className="card-title">{superhero}</h5>
-              <p className="card-text">{alter_ego}</p>
-              <CharactersByHero characters={characters} alter_ego={alter_ego}/>
-              <p className="card-text">
-                <small className="text-muted">{first_appearance}</small>
-              </p>
-
-              <Link to={`/hero/${id}`}>Mas Info...</Link>
-            </div>
+    <div className="card">
+      <div className="row no-gutters">
+        <div className="col-md-4 col-12">
+          <img src={heroImgUrl} alt={superhero} className="card-img" />
+        </div>
+  
+        <div className="col-md-8 col-12 col-md-6">
+          <div className="card-body">
+            <h5 className="card-title">{superhero}</h5>
+            <p className="card-text">{alter_ego}</p>
+            <CharactersByHero characters={characters} alter_ego={alter_ego}/>
+            <p className="card-text">
+              <small className="text-muted">{first_appearance}</small>
+            </p>
+            <Link to={`/hero/${id}`}>Mas Info...</Link>
           </div>
         </div>
       </div>
     </div>
+  </div>
+  
+  
   );
 };
